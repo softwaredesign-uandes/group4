1dae8102701d65a95c4f9fd6c8a3bb44b817d77c
Sebastian Baixas <sbaixas@gmail.com>
2019-03-31 20:05:31 -0300

    Created Block class to contain generic and dynamic data from both models. Created FileReader class to parse from both formats of .block files into Block objects. Created BlockModel class to contain blocks and call FileReader functions according to the specified format, and some methods to do basic queries to the model. Console Output class is a work in progress.

---
+++ BlockModelReader/BlockModelReader/Block.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockModelReader
+{
+    class Block
+    {
+        private int id, xCoordinate, yCoordinate, zCoordinate;
+        private double weight;
+        private Dictionary<string, double> grades;
+        private Dictionary<string, double> additionalData;
+        public Block(int id, int xCoordinate, int yCoordinate, int zCoordinate, double weight)
+        {
+            this.id = id;
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+            this.zCoordinate = zCoordinate;
+            this.grades = new Dictionary<string, double>();
+            this.additionalData = new Dictionary<string, double>();
+        }
+
+        public Block(int id, int xCoordinate, int yCoordinate, int zCoordinate, double weight, Dictionary<string, double> grades)
+        {
+            this.id = id;
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+            this.zCoordinate = zCoordinate;
+            this.grades = grades;
+            this.additionalData = new Dictionary<string, double>();
+        }
+
+        public Block(int id, int xCoordinate, int yCoordinate, int zCoordinate, double weight, Dictionary<string, double> grades, Dictionary<string, double> additionalData)
+        {
+            this.id = id;
+            this.xCoordinate = xCoordinate;
+            this.yCoordinate = yCoordinate;
+            this.zCoordinate = zCoordinate;
+            this.grades = grades;
+            this.additionalData = additionalData;
+        }
+
+        public void SetGrade(string key, double value)
+        {
+            grades[key] = value;
+        }
+
+        public void SetAdditionalData(string key, double value)
+        {
+            additionalData[key] = value;
+        }
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public int[] GetCoordinates()
+        {
+            return new [] { xCoordinate, yCoordinate, zCoordinate };
+        }
+    }
+}
---
+++ BlockModelReader/BlockModelReader/FileReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BlockModelReader
+{
+    static class FileReader
+    {
+        public static List<Block> ReadMarvinFile(string path)
+        {
+            List<Block> result = new List<Block>();
+            foreach (string line in File.ReadLines(path, Encoding.UTF8))
+            {
+                string[] lineValues = line.Split(' ');
+                int id = int.Parse(lineValues[0]);
+                int xCoordinate = int.Parse(lineValues[1]);
+                int yCoordinate = int.Parse(lineValues[2]);
+                int zCoordinate = int.Parse(lineValues[3]);
+                double weight = double.Parse(lineValues[4]);
+                Dictionary<string, double> grades = new Dictionary<string, double>();
+                grades["Au"] = double.Parse(lineValues[5]) / 10000;
+                grades["Cu"] = double.Parse(lineValues[6]);
+                Dictionary<string, double> additionalData = new Dictionary<string, double>();
+                additionalData["proc_profit"] = double.Parse(lineValues[7]);
+                result.Add(new Block(id, xCoordinate, yCoordinate, zCoordinate, weight, grades, additionalData));
+            }
+            return result;
+        }
+
+        public static List<Block> ReadZuccSmallFile(string path)
+        {
+            List<Block> result = new List<Block>();
+            foreach (string line in File.ReadLines(path, Encoding.UTF8))
+            {
+                string[] lineValues = line.Split(' ');
+                int id = int.Parse(lineValues[0]);
+                int xCoordinate = int.Parse(lineValues[1]);
+                int yCoordinate = int.Parse(lineValues[2]);
+                int zCoordinate = int.Parse(lineValues[3]);
+                double rockWeight = double.Parse(lineValues[6]);
+                double oreWeight = double.Parse(lineValues[7]);
+                double weight = rockWeight + oreWeight;
+                Dictionary<string, double> grades = new Dictionary<string, double>();
+                grades["Ore"] = oreWeight / (oreWeight + rockWeight);
+                Dictionary<string, double> additionalData = new Dictionary<string, double>();
+                additionalData["cost"] = double.Parse(lineValues[4]);
+                additionalData["value"] = double.Parse(lineValues[5]);
+                additionalData["rock_tonnes"] = rockWeight;
+                additionalData["ore_tonnes"] = oreWeight;
+                result.Add(new Block(id, xCoordinate, yCoordinate, zCoordinate, weight, grades, additionalData));
+            }
+            return result;
+        }
+    }
+}
