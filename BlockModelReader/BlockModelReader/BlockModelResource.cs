c51d1a86ba61b8088e3d7149412faeb3386a1594
Sebastian Baixas <sbaixas@gmail.com>
2019-06-03 01:59:48 -0400

    Added error management to the RESTful routes and fixed a typo in a post response

--- BlockModelReader/BlockModelReader/BlockModelResource.cs
+++ BlockModelReader/BlockModelReader/BlockModelResource.cs
@@ -19,148 +19,187 @@ namespace BlockModelReader
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/block_models/")]
         public IHttpContext GetBlockModels(IHttpContext context)
         {
-            dynamic jsonResponse = new JObject();
-            JObject[] block_models = MineralDepositsEnvironment.GetBlockModels().Select(blockModel =>
+            try
             {
-            dynamic block_model = new JObject();
-            block_model.id = blockModel.GetId();
-            return block_model as JObject;
-            }).ToArray();
-            jsonResponse.block_models = new JArray(block_models);
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+                dynamic jsonResponse = new JObject();
+                JObject[] block_models = MineralDepositsEnvironment.GetBlockModels().Select(blockModel =>
+                {
+                    dynamic block_model = new JObject();
+                    block_model.id = blockModel.GetId();
+                    return block_model as JObject;
+                }).ToArray();
+                jsonResponse.block_models = new JArray(block_models);
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
+            }
+            catch(Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/block_models/[id]")]
         public IHttpContext GetBlockModel(IHttpContext context)
         {
-            int id = int.Parse(context.Request.PathParameters["id"]);
-            context.Response.ContentType = ContentType.JSON;
-            BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
-            dynamic jsonResponse = new JObject();
-            jsonResponse.block_model = new JObject();
-            jsonResponse.block_model.id = id;
-            jsonResponse.block_model.total_weight = blockModel.GetTotalWeight(blockModel.GetBlocks());
-            jsonResponse.block_model.air_blocks_percentage = blockModel.CalculateAirBlocksPercentage(blockModel.GetBlocks());
-
-            Dictionary<string, double> mineralWeights = blockModel.CalculateMineralWeights(blockModel.GetBlocks());
-            jsonResponse.block_model.mineral_weights = new JArray();
-            foreach(string key in mineralWeights.Keys)
+            try
             {
-                JObject mineral_weight = JObject.Parse("{\"" + key + "\": " + mineralWeights[key].ToString() + " }");
-                jsonResponse.block_model.mineral_weights.Add(mineral_weight);
-            }
+                int id = int.Parse(context.Request.PathParameters["id"]);
+                context.Response.ContentType = ContentType.JSON;
+                BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
+                dynamic jsonResponse = new JObject();
+                jsonResponse.block_model = new JObject();
+                jsonResponse.block_model.id = id;
+                jsonResponse.block_model.total_weight = blockModel.GetTotalWeight(blockModel.GetBlocks());
+                jsonResponse.block_model.air_blocks_percentage = blockModel.CalculateAirBlocksPercentage(blockModel.GetBlocks());
 
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+                Dictionary<string, double> mineralWeights = blockModel.CalculateMineralWeights(blockModel.GetBlocks());
+                jsonResponse.block_model.mineral_weights = new JArray();
+                foreach (string key in mineralWeights.Keys)
+                {
+                    JObject mineral_weight = JObject.Parse("{\"" + key + "\": " + mineralWeights[key].ToString() + " }");
+                    jsonResponse.block_model.mineral_weights.Add(mineral_weight);
+                }
+
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
+            }
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
 
         [RestRoute(HttpMethod = HttpMethod.POST, PathInfo = "/block_models/")]
         public IHttpContext CreateBlockModel(IHttpContext context)
         {
-            context.Response.ContentType = ContentType.JSON;
-            dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
-            if (payload.block_model.x_indices != null)
+            try
             {
-                int[] xIndices = payload.block_model.x_indices.ToObject<int[]>();
-                int[] yIndices = payload.block_model.y_indices.ToObject<int[]>();
-                int[] zIndices = payload.block_model.z_indices.ToObject<int[]>();
-                double[] weights = payload.block_model.z_indices.ToObject<double[]>();
-                Dictionary<string, double[]> grades = payload.block_model.grades.ToObject<Dictionary<string, double[]>>();
-                int[] blockIterations = Enumerable.Range(0, xIndices.Length).ToArray();
-                List<IReblockable> blocks = blockIterations.Select(i =>
+                context.Response.ContentType = ContentType.JSON;
+                dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
+                if (payload.block_model.x_indices != null)
                 {
-                    IReblockable block = new Block(xIndices[i], yIndices[i], zIndices[i], weights[i]);
-                    foreach (string key in grades.Keys)
+                    int[] xIndices = payload.block_model.x_indices.ToObject<int[]>();
+                    int[] yIndices = payload.block_model.y_indices.ToObject<int[]>();
+                    int[] zIndices = payload.block_model.z_indices.ToObject<int[]>();
+                    double[] weights = payload.block_model.z_indices.ToObject<double[]>();
+                    Dictionary<string, double[]> grades = payload.block_model.grades.ToObject<Dictionary<string, double[]>>();
+                    int[] blockIterations = Enumerable.Range(0, xIndices.Length).ToArray();
+                    List<IReblockable> blocks = blockIterations.Select(i =>
                     {
-                        ((Block)block).SetGrade(key, grades[key][i]);
-                    }
-                    return block;
-                }).ToList();
-                BlockModel blockModel = new BlockModel();
-                blockModel.SetBlocks(blocks);
-                MineralDepositsEnvironment.AddBlockModel(blockModel);
-                context.Response.SendResponse("Successfuly Added Block Model");
+                        IReblockable block = new Block(xIndices[i], yIndices[i], zIndices[i], weights[i]);
+                        foreach (string key in grades.Keys)
+                        {
+                            ((Block)block).SetGrade(key, grades[key][i]);
+                        }
+                        return block;
+                    }).ToList();
+                    BlockModel blockModel = new BlockModel();
+                    blockModel.SetBlocks(blocks);
+                    MineralDepositsEnvironment.AddBlockModel(blockModel);
+                    context.Response.SendResponse("Successfuly Added Block Model");
+                }
+                else if (payload.block_model.base_block_model_id != null)
+                {
+                    int blockModelId = payload.block_model.base_block_model_id.ToObject<int>();
+                    int reblockX = payload.block_model.reblock_x.ToObject<int>();
+                    int reblockY = payload.block_model.reblock_y.ToObject<int>();
+                    int reblockZ = payload.block_model.reblock_z.ToObject<int>();
+                    BlockModel blockModel = new BlockModel();
+                    blockModel.SetBlocks(MineralDepositsEnvironment.GetBlockModel(blockModelId).ReBlock(reblockX, reblockY, reblockZ, true));
+                    MineralDepositsEnvironment.AddBlockModel(blockModel);
+                    context.Response.SendResponse("Successfuly Added Reblocked Block Model");
+                }
+                MineralDepositsEnvironment.SerializePersistentData();
+                return context;
             }
-            else if(payload.block_model.base_block_model_id != null)
+            catch (Exception e)
             {
-                int blockModelId = payload.block_model.base_block_model_id.ToObject<int>();
-                int reblockX = payload.block_model.reblock_x.ToObject<int>();
-                int reblockY = payload.block_model.reblock_y.ToObject<int>();
-                int reblockZ = payload.block_model.reblock_z.ToObject<int>();
-                BlockModel blockModel = new BlockModel();
-                blockModel.SetBlocks(MineralDepositsEnvironment.GetBlockModel(blockModelId).ReBlock(reblockX, reblockY, reblockZ, true));
-                MineralDepositsEnvironment.AddBlockModel(blockModel);
-                context.Response.SendResponse("Successfuly Added Reblocked Block Model");
+                context.Response.SendResponse(e.ToString());
+                return context;
             }
-            MineralDepositsEnvironment.SerializePersistentData();
-            return context;
         }
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/block_models/[id]/blocks")]
         public IHttpContext GetBlocks(IHttpContext context)
         {
-            int id = int.Parse(context.Request.PathParameters["id"]);
-            context.Response.ContentType = ContentType.JSON;
-            BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
-            dynamic jsonResponse = new JObject();
-            JObject[] blocks = blockModel.GetBlocks().Select(block =>
+            try
             {
-                int[] coordinates = block.GetCoordinates();
-                dynamic j_block = new JObject();
-                j_block.id = block.GetId();
-                j_block.x_index = coordinates[0];
-                j_block.y_index = coordinates[1];
-                j_block.z_index = coordinates[2];
-                j_block.weight = block.GetWeight();
-                Dictionary<string, double> grades = block.GetGrades();
-                j_block.grades = new JArray();
-                foreach (string key in grades.Keys)
+                int id = int.Parse(context.Request.PathParameters["id"]);
+                context.Response.ContentType = ContentType.JSON;
+                BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
+                dynamic jsonResponse = new JObject();
+                JObject[] blocks = blockModel.GetBlocks().Select(block =>
                 {
-                    JObject grade = JObject.Parse("{\"" + key + "\": " + grades[key].ToString() + " }");
-                    j_block.grades.Add(grade);
-                }
-                return j_block as JObject;
-            }).ToArray();
-            jsonResponse.blocks = new JArray(blocks);
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+                    int[] coordinates = block.GetCoordinates();
+                    dynamic j_block = new JObject();
+                    j_block.id = block.GetId();
+                    j_block.x_index = coordinates[0];
+                    j_block.y_index = coordinates[1];
+                    j_block.z_index = coordinates[2];
+                    j_block.weight = block.GetWeight();
+                    Dictionary<string, double> grades = block.GetGrades();
+                    j_block.grades = new JArray();
+                    foreach (string key in grades.Keys)
+                    {
+                        JObject grade = JObject.Parse("{\"" + key + "\": " + grades[key].ToString() + " }");
+                        j_block.grades.Add(grade);
+                    }
+                    return j_block as JObject;
+                }).ToArray();
+                jsonResponse.blocks = new JArray(blocks);
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
+            }
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/block_models/[id]/blocks/[block_id]")]
         public IHttpContext GetBlock(IHttpContext context)
         {
-            int id = int.Parse(context.Request.PathParameters["id"]);
-            int blockId = int.Parse(context.Request.PathParameters["block_id"]);
-            context.Response.ContentType = ContentType.JSON;
-            BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
-            dynamic jsonResponse = new JObject();
-            IReblockable block = blockModel.SimpleIdQuery(blockId);
-            int[] coordinates = block.GetCoordinates();
-            jsonResponse.block = new JObject();
-            jsonResponse.block.id = block.GetId();
-            jsonResponse.block.x_index = coordinates[0];
-            jsonResponse.block.y_index = coordinates[1];
-            jsonResponse.block.z_index = coordinates[2];
-            jsonResponse.block.weight = block.GetWeight();
-            Dictionary<string, double> grades = block.GetGrades();
-            jsonResponse.block.grades = new JArray();
-            foreach (string key in grades.Keys)
+            try
             {
-                JObject grade = JObject.Parse("{\"" + key + "\": " + grades[key].ToString() + " }");
-                jsonResponse.block.grades.Add(grade);
+                int id = int.Parse(context.Request.PathParameters["id"]);
+                int blockId = int.Parse(context.Request.PathParameters["block_id"]);
+                context.Response.ContentType = ContentType.JSON;
+                BlockModel blockModel = MineralDepositsEnvironment.GetBlockModel(id);
+                dynamic jsonResponse = new JObject();
+                IReblockable block = blockModel.SimpleIdQuery(blockId);
+                int[] coordinates = block.GetCoordinates();
+                jsonResponse.block = new JObject();
+                jsonResponse.block.id = block.GetId();
+                jsonResponse.block.x_index = coordinates[0];
+                jsonResponse.block.y_index = coordinates[1];
+                jsonResponse.block.z_index = coordinates[2];
+                jsonResponse.block.weight = block.GetWeight();
+                Dictionary<string, double> grades = block.GetGrades();
+                jsonResponse.block.grades = new JArray();
+                foreach (string key in grades.Keys)
+                {
+                    JObject grade = JObject.Parse("{\"" + key + "\": " + grades[key].ToString() + " }");
+                    jsonResponse.block.grades.Add(grade);
+                }
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
             }
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
-
     }
 }
--- BlockModelReader/BlockModelReader/MineralDepositResource.cs
+++ BlockModelReader/BlockModelReader/MineralDepositResource.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Grapevine.Interfaces.Server;
 using Grapevine.Server;
@@ -16,102 +17,79 @@ namespace BlockModelReader
         [RestRoute(HttpMethod = HttpMethod.POST, PathInfo = "/mineral_deposits/")]
         public IHttpContext CreateMineralDeposit(IHttpContext context)
         {
-            context.Response.ContentType = ContentType.JSON;
-            dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
-            string name = payload.mineral_deposit.ToObject<Dictionary<string, string>>()["name"];
-            MineralDepositsEnvironment.AddMineralDeposit(name);
-            context.Response.SendResponse("Successfuly Mineral Deposit Added");
-            MineralDepositsEnvironment.SerializePersistentData();
-            return context;
+            try
+            {
+                context.Response.ContentType = ContentType.JSON;
+                dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
+                string name = payload.mineral_deposit.ToObject<Dictionary<string, string>>()["name"];
+                MineralDepositsEnvironment.AddMineralDeposit(name);
+                context.Response.SendResponse("Mineral Deposit Successfuly Added");
+                MineralDepositsEnvironment.SerializePersistentData();
+                return context;
+            }
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/mineral_deposits/")]
         public IHttpContext GetMineralDeposits(IHttpContext context)
         {
-            dynamic jsonResponse = new JObject();
-            JObject[] mineral_deposits = MineralDepositsEnvironment.GetMineralDeposits().Select(mineralDeposit =>
+            try
             {
-                dynamic mineral_deposit = new JObject();
-                mineral_deposit.id = mineralDeposit.GetId();
-                mineral_deposit.name = mineralDeposit.GetName();
-                return mineral_deposit as JObject;
-            }).ToArray();
-            jsonResponse.mineral_deposits = new JArray(mineral_deposits);
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+                dynamic jsonResponse = new JObject();
+                JObject[] mineral_deposits = MineralDepositsEnvironment.GetMineralDeposits().Select(mineralDeposit =>
+                {
+                    dynamic mineral_deposit = new JObject();
+                    mineral_deposit.id = mineralDeposit.GetId();
+                    mineral_deposit.name = mineralDeposit.GetName();
+                    return mineral_deposit as JObject;
+                }).ToArray();
+                jsonResponse.mineral_deposits = new JArray(mineral_deposits);
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
+            }
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
 
 
         [RestRoute(HttpMethod = HttpMethod.GET, PathInfo = "/mineral_deposits/[id]")]
         public IHttpContext GetMineralDeposit(IHttpContext context)
         {
-            int id = int.Parse(context.Request.PathParameters["id"]);
-            context.Response.ContentType = ContentType.JSON;
-            MineralDeposit mineralDeposit = MineralDepositsEnvironment.GetMineralDeposit(id);
-            dynamic jsonResponse = new JObject();
-            jsonResponse.mineral_deposit = new JObject();
-            jsonResponse.mineral_deposit.id = id;
-            jsonResponse.mineral_deposit.name = mineralDeposit.GetName();
-            JObject[] block_models = mineralDeposit.GetBlockModels().Select(blockModel =>
+            try
             {
-                dynamic mineral_deposit = new JObject();
-                mineral_deposit.id = blockModel.GetId();
-                return mineral_deposit as JObject;
-            }).ToArray();
-            jsonResponse.mineral_deposit.block_models = new JArray(block_models);
-            string responseString = jsonResponse.ToString();
-            context.Response.ContentType = ContentType.JSON;
-            context.Response.SendResponse(responseString);
-            return context;
+                int id = int.Parse(context.Request.PathParameters["id"]);
+                context.Response.ContentType = ContentType.JSON;
+                MineralDeposit mineralDeposit = MineralDepositsEnvironment.GetMineralDeposit(id);
+                dynamic jsonResponse = new JObject();
+                jsonResponse.mineral_deposit = new JObject();
+                jsonResponse.mineral_deposit.id = id;
+                jsonResponse.mineral_deposit.name = mineralDeposit.GetName();
+                JObject[] block_models = mineralDeposit.GetBlockModels().Select(blockModel =>
+                {
+                    dynamic mineral_deposit = new JObject();
+                    mineral_deposit.id = blockModel.GetId();
+                    return mineral_deposit as JObject;
+                }).ToArray();
+                jsonResponse.mineral_deposit.block_models = new JArray(block_models);
+                string responseString = jsonResponse.ToString();
+                context.Response.ContentType = ContentType.JSON;
+                context.Response.SendResponse(responseString);
+                return context;
+            }
+            catch (Exception e)
+            {
+                context.Response.SendResponse(e.ToString());
+                return context;
+            }
         }
-
-
-
-        /* [RestRoute(HttpMethod = HttpMethod.POST, PathInfo = "/load_settings")]
-         public IHttpContext LoadSettings(IHttpContext context)
-         {
-             try
-             {
-                 context.Response.ContentType = ContentType.JSON;
-                 dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
-                 string weightFormula = payload.weigth_formula;
-                 FileReader.SetWeightExpression(weightFormula);
-                 FileReader.SetOreNames(payload.ore_names.ToObject<string[]>());
-                 FileReader.SetOreExpressions(payload.ore_formulas.ToObject<string[]>());
-                 FileReader.ClearData();
-                 context.Response.SendResponse("OK");
-
-             }
-             catch (Exception e)
-             {
-                 throw e;
-             }
-             return context;
-         }
-         [RestRoute(HttpMethod = HttpMethod.POST, PathInfo = "/load_sheet")]
-         public IHttpContext LoadSheet(IHttpContext context)
-         {
-                 context.Response.ContentType = ContentType.JSON;
-                 dynamic payload = JsonConvert.DeserializeObject(context.Request.Payload);
-                 double[][] dataSegment = payload.data.ToObject<double[][]>();
-                 foreach (double[] d in dataSegment)
-                 {
-                     FileReader.AddData(d);
-                 }
-                 if (payload.last == "true")
-                 {
-                     context.Response.SendResponse("Finished");
-                     MineralDeposit.SetBlocks(FileReader.ReadList());
-                     Console.WriteLine("CHECK");
-                 }
-                 else
-                 {
-                     context.Response.SendResponse("OK");
-                 }
-
-             return context;
-         }*/
      }
 }
